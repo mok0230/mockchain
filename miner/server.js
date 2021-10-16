405a8f53be78edf4ad12d21de7c120631414e33b
mok <mok0230@protonmail.com>
2021-10-15 21:13:00 -0700

    accept both port and peers on command line

--- miner/server.js
+++ miner/server.js
@@ -1,12 +1,19 @@
+#!/usr/bin/env node
+// sample CLI arguments
+// node miner/server.js --peers 3001 --peers 3002 --port 3000
+
 const express = require('express');
 const app = express();
-// const port = 3000;
 // const SHA256 = require('crypto-js/sha256');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv;
 
-const port = { argv };
+const port = argv.port;
+
+if (!port) {
+  throw new Error("port is required")
+}
 
 if (argv.peers) {
   console.log('Peers!', argv.peers);
