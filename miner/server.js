fc184c953c376b40e4c7116034c89a6b17748419
mok <mok0230@protonmail.com>
2021-10-15 20:41:04 -0700

    add yargs and simple command line parsing to miner server

--- miner/server.js
+++ miner/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
 // const SHA256 = require('crypto-js/sha256');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+const port = { argv };
+
+if (argv.peers) {
+  console.log('Peers!', argv.peers);
+} else {
+  console.log('No peers defined!')
+}
 
 app.use(express.json());
 
