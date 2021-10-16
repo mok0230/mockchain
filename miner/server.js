481a9b70c020fb033bd94a6c127fd100a2e991d5
mok <mok0230@protonmail.com>
2021-10-15 22:54:45 -0700

    initialize blockchain for first miner

--- miner/server.js
+++ miner/server.js
@@ -3,14 +3,16 @@
 // node miner/server.js --peers 3001 --peers 3002 --port 3000
 
 const express = require('express');
-const app = express();
 // const SHA256 = require('crypto-js/sha256');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
+const { genesisBlock } = require('./utils');
+const { Blockchain } = require('./models/Blockchain');
 const argv = yargs(hideBin(process.argv)).argv;
+const app = express();
+app.use(express.json());
 
 const port = argv.port;
-const peers = [];
 
 if (!port) {
   throw new Error("port is required")
@@ -18,14 +20,16 @@ if (!port) {
 
 console.log('Starting server')
 
-if (argv.peers) {
-  argv.peers.forEach(peer => peers.push(peer));
-  console.log('Added peers', peers);
-} else {
-  console.log('No peers defined!')
-}
+// the only node that has no peers is the first one
+const isSatoshi = Boolean(argv.peers);
 
-app.use(express.json());
+const address = isSatoshi ? 'satoshi' : port.toString();
+
+const peers = argv.peers || [];
+
+const blockchain = new Blockchain(isSatoshi ? genesisBlock : null);
+
+console.log('blockchain.blocks', blockchain.blocks);
 
 app.get('/data', (req, res) => {
   // TODO: return current blockchain
