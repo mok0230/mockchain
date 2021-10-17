bdf9d43ee1038f36b3c6b83012e52d052158c1f2
mok <mok0230@protonmail.com>
2021-10-16 23:30:48 -0700

    add skeleton client with ionic framework

---
+++ client/index.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const state = {
+    views: ['explorer', 'balances', 'hashrates'],
+    activeView: 'explorer'
+  }
+
+  function activateView(activatedView) {
+    document.querySelector(`#${state.activeView}`).classList.add('ion-hide');
+    document.querySelector(`ion-item[name="${state.activeView}"]`).removeAttribute('color');
+    document.querySelector(`#${activatedView}`).classList.remove('ion-hide');
+    document.querySelector(`ion-item[name="${activatedView}"]`).setAttribute('color', 'dark');
+    state.activeView = activatedView;
+  }
+  
+  document.querySelector('#side-nav').addEventListener('click', e => {
+    const activatedItem = e.target.closest('ion-item').getAttribute('name');
+    if (activatedItem != state.activeView) {
+      activateView(activatedItem);
+    }
+    
+  });
+});
